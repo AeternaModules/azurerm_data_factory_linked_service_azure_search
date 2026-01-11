03feb654c83f1899d8f695ce3caa55c755cf6791
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:44:18 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_data_factory_linked_service_azure_search" "data_factory_linked_service_azure_searchs" {
+  for_each = var.data_factory_linked_service_azure_searchs
+
+  data_factory_id          = each.value.data_factory_id
+  name                     = each.value.name
+  search_service_key       = each.value.search_service_key
+  url                      = each.value.url
+  additional_properties    = each.value.additional_properties
+  annotations              = each.value.annotations
+  description              = each.value.description
+  integration_runtime_name = each.value.integration_runtime_name
+  parameters               = each.value.parameters
+}
+
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_azure_searchs" {
+  description = "All data_factory_linked_service_azure_search resources"
+  value       = azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searchs
+}
+
