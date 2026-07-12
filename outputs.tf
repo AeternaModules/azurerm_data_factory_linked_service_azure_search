e07a1ea39455aa12bf63437412c18b7327c76f11
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "data_factory_linked_service_azure_searches" {
-  description = "All data_factory_linked_service_azure_search resources"
-  value       = azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches
-}
 output "data_factory_linked_service_azure_searches_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.additional_properties }
 }
 output "data_factory_linked_service_azure_searches_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.annotations }
 }
 output "data_factory_linked_service_azure_searches_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.data_factory_id }
 }
 output "data_factory_linked_service_azure_searches_description" {
-  description = "List of description values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.description]
+  description = "Map of description values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.description }
 }
 output "data_factory_linked_service_azure_searches_encrypted_credential" {
-  description = "List of encrypted_credential values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.encrypted_credential]
+  description = "Map of encrypted_credential values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.encrypted_credential }
 }
 output "data_factory_linked_service_azure_searches_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_azure_searches_name" {
-  description = "List of name values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.name]
+  description = "Map of name values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.name }
 }
 output "data_factory_linked_service_azure_searches_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.parameters }
 }
 output "data_factory_linked_service_azure_searches_search_service_key" {
-  description = "List of search_service_key values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.search_service_key]
+  description = "Map of search_service_key values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.search_service_key }
 }
 output "data_factory_linked_service_azure_searches_url" {
-  description = "List of url values across all data_factory_linked_service_azure_searches"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : v.url]
+  description = "Map of url values across all data_factory_linked_service_azure_searches, keyed the same as var.data_factory_linked_service_azure_searches"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_search.data_factory_linked_service_azure_searches : k => v.url }
 }
 
